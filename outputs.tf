09b314b9d596645945ff3363d40c80783b78557e
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-20 14:05:28 -0500

    Update to return a map instead of a list
    This way we can add/remove/modify subnets without having to recreate
    all the resources.
    
    Also go ahead and return the actual resources and not just IDs.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "subnet_ids" {
-  value       = aws_subnet.the_subnets[*].id
-  description = "The subnet IDs"
+output "subnets" {
+  value       = aws_subnet.the_subnets
+  description = "A map with keys equal to the subnet CIDR blocks and values equal to the subnets."
 }
