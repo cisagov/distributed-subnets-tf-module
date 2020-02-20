cc2890012ba83916dcb003f13e02e3d19b5a1c98
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-20 14:43:28 -0500

    End descriptions with a period
    Thanks to @mcdonnnj for pointing this out!

--- examples/basic_usage/outputs.tf
+++ examples/basic_usage/outputs.tf
@@ -1,14 +1,14 @@
 output "vpc" {
   value       = aws_vpc.the_vpc
-  description = "The VPC"
+  description = "The VPC."
 }
 
 output "private_subnets" {
   value       = module.private_subnets.subnets
-  description = "The private subnets"
+  description = "The private subnets."
 }
 
 output "public_subnets" {
   value       = module.public_subnets.subnets
-  description = "The public subnets"
+  description = "The public subnets."
 }
